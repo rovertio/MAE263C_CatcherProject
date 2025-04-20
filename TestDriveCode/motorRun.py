372b89229ab406d01a0216d0d682a6bc6303ace0
rovertio <73990195+rovertio@users.noreply.github.com>
2025-04-19 19:57:00 -0700

    python update

--- TestDriveCode/motorRun.py
+++ TestDriveCode/motorRun.py
@@ -165,14 +165,15 @@ class PIDPositionController:
     def signal_handler(self, *_):
         self.stop()
 
-class motorControl:
+class motorControlAn:
     """
     This class manages a PID Position Controller
     """
 
     def __init__(
         self,
-        motor: dxl.DynamixelMotor,
+        motorS: dxl.DynamixelMotor,
+        motorE: dxl.DynamixelMotor,
         proportional_gain: float,
         integral_gain: float = 0.0,
         derivative_gain: float = 0.0,
@@ -181,7 +182,8 @@ class motorControl:
         max_duration_s: float = 2.0,
     ):
         self.max_duration_s = float(max_duration_s)
-        self.motor = motor
+        self.motorS = motorS
+        self.motorE = motorE
         self.should_continue = True
 
         # `FixedFrequencyLoopManager` trys keep loop at a fixed frequency
@@ -189,7 +191,7 @@ class motorControl:
 
         # Set the position set-point in units of degrees
         self.position_set_point_deg = max(
-            min(ang_set_point_deg, motor.model_info.max_angle_deg), 0.0
+            min(ang_set_point_deg, motorE.model_info.max_angle_deg), 0.0
         )
 
         # Set PID gains
@@ -211,20 +213,28 @@ class motorControl:
         self._timestamps = deque()
 
         # Put motor in "home" posiiton
-        self.motor.set_mode(DynamixelMode.Position)
-        self.motor.enable_torque()
-        self.motor.angle_deg = 0.0
-        while abs(self.motor.angle_deg) > 0.8:
-            self.motor.angle_deg = 0.0
+        self.motorE.set_mode(DynamixelMode.Position)
+        self.motorS.set_mode(DynamixelMode.Position)
+        self.motorE.enable_torque()
+        self.motorS.enable_torque()
+        self.motorE.angle_deg = 0.0
+        self.motorS.angle_deg = 0.0
+        while abs(self.motorE.angle_deg) > 0.8 or abs(self.motorS.angle_deg) > 0.8:
+            self.motorE.angle_deg = 0.0
+            self.motorS.angle_deg = 0.0
             time.sleep(0.5)
 
-        self.motor.disable_torque()
+        self.motorE.disable_torque()
+        self.motorS.disable_torque()
         time.sleep(0.5)
 
         # PWM Mode (i.e. voltage control)
-        self.pwm_limit = float(self.motor.motor_info.pwm_limit)
-        motor.set_mode(DynamixelMode.PWM)
+        self.pwm_limit = float(self.motorE.motor_info.pwm_limit)
+        self.pwm_limit = float(self.motorS.motor_info.pwm_limit)
+        # motorE.set_mode(DynamixelMode.PWM)
+        # motorS.set_mode(DynamixelMode.PWM)
 
+    # Defining position values to be stored
     @property
     def position_history(self) -> NDArray[np.double]:
         return np.asarray(self._position_history)
@@ -236,73 +246,34 @@ class motorControl:
         return t
 
     def start(self):
-        self.motor.enable_torque()
+        self.motorE.enable_torque()
+        self.motorS.enable_torque()
         curr_time = time.time_ns()
-        while self.should_continue:
-            # --------------------------------------------------------------------------
-            # Step 1 - Get Feedback
-            # --------------------------------------------------------------------------
-            ang_deg = self.motor.angle_deg
+        # while self.should_continue:
+        #     # angE_deg = self.motorE.angle_deg
+        #     angS_deg = self.motorS.angle_deg
 
-            self._position_history.append(ang_deg)  # Save for plotting
-            self._timestamps.append(time.time())  # Save for plotting
+        #     self._position_history.append(angS_deg)  # Save for plotting
+        #     self._timestamps.append(time.time())  # Save for plotting
 
-            # --------------------------------------------------------------------------
-            # Step 2 - Update PID Terms
-            # --------------------------------------------------------------------------
-            self.error = self.position_set_point_deg - ang_deg
+        #     # Stop after 2 seconds
+        #     if self._timestamps[-1] - self._timestamps[0] > self.max_duration_s:
+        #         self.motor.disable_torque()
+        #         return
 
-            self.error_window.append(self.error)
+        #     # Print current position in degrees
+        #     print("Current Position:", self.motor.angle_deg)
 
-            prev_time = curr_time
-            curr_time = time.time_ns()
+        #     self.loop_manager.sleep()
+        self.motorE.angle_deg = 0.0
+        self.motorS.angle_deg = 0.0
+        time.sleep(0.5)
 
-            self.error_integral += self.error * (curr_time - prev_time) / 1e9
+        self.motorE.angle_deg = 90.0
+        self.motorS.angle_deg = 90.0
+        time.sleep(0.5)
 
-            if len(self.error_window) == self.error_window.maxlen:
-                self.error_derivative = (
-                    (
-                        3 * self._position_history[-1]
-                        - 4 * self._position_history[-2]
-                        + self._position_history[-3]
-                    )
-                    / 2
-                    / ((curr_time - prev_time) / 1e9)
-                )
-                self.error_derivative = -self.filter(self.error_derivative)
-            elif len(self.error_window) == 2:
-                self.error_derivative = -self.filter(
-                    (self._position_history[-1] - self._position_history[-2])
-                    / ((curr_time - prev_time) / 1e9)
-                )
-            else:
-                self.error_derivative = self.filter(0.0)
-
-            # --------------------------------------------------------------------------
-            # Step 3 - Check termination criterion
-            # --------------------------------------------------------------------------
-            # Stop after 2 seconds
-            if self._timestamps[-1] - self._timestamps[0] > self.max_duration_s:
-                self.motor.disable_torque()
-                return
-
-            # --------------------------------------------------------------------------
-            # Step 4 - Calculate and send command
-            # --------------------------------------------------------------------------
-            pwm_command = (
-                self.proportional_gain * self.error
-                + self.integral_gain * self.error_integral
-                + self.derivative_gain * self.error_derivative
-            )
-
-            # Saturate control action (pwm duty cycle in the range [-100.0, 100.0])
-            pwm_command = max(min(pwm_command, 100.0), -100.0)
-            self.motor.pwm_percentage = pwm_command
-
-            # Print current position in degrees
-            print("Current Position:", self.motor.angle_deg)
-
-            self.loop_manager.sleep()
+        
 
         self.motor.disable_torque()
 
@@ -333,12 +304,14 @@ if __name__ == "__main__":
     )
 
     # TODO: Replace "..." below with the correct Dynamixel ID found from Dynamixel Wizard
-    motor = motor_factory.create(4)
+    motorE = motor_factory.create(4)
+    motorS = motor_factory.create(2)
 
     # Make controller
     # TODO: Replace all "..." below with your selected choices of gains.
-    controller = PIDPositionController(
-        motor=motor,
+    controller = motorControlAn(
+        motorE=motorE,
+        motorS=motorS,
         proportional_gain=1,
         integral_gain=0.001,
         derivative_gain=0.001,
@@ -351,37 +324,37 @@ if __name__ == "__main__":
     # ----------------------------------------------------------------------------------
     # Plot Results
     # ----------------------------------------------------------------------------------
-    gains = (
-        controller.proportional_gain,
-        controller.integral_gain,
-        controller.derivative_gain,
-    )
-    fig_file_name = f"p{gains[0]}-i{gains[1]}-d{gains[2]}.pdf"
+    # gains = (
+    #     controller.proportional_gain,
+    #     controller.integral_gain,
+    #     controller.derivative_gain,
+    # )
+    # fig_file_name = f"p{gains[0]}-i{gains[1]}-d{gains[2]}.pdf"
 
-    # Create figure and axes
-    fig = plt.figure(figsize=(10, 5))
-    ax = fig.add_subplot(111)
+    # # Create figure and axes
+    # fig = plt.figure(figsize=(10, 5))
+    # ax = fig.add_subplot(111)
 
-    # Plot setpoint of 90.0 angle trajectory (with label)
-    ax.set_title(
-        f"Motor Angle vs Time ($K_p$={gains[0]}, $K_i$={gains[1]}, $K_d$={gains[2]})"
-    )
-    ax.set_xlabel("Time [s]")
-    ax.set_ylabel("Motor Angle [deg]")
+    # # Plot setpoint of 90.0 angle trajectory (with label)
+    # ax.set_title(
+    #     f"Motor Angle vs Time ($K_p$={gains[0]}, $K_i$={gains[1]}, $K_d$={gains[2]})"
+    # )
+    # ax.set_xlabel("Time [s]")
+    # ax.set_ylabel("Motor Angle [deg]")
 
-    # Plot setpoint of 90.0 angle trajectory (with label)
-    ax.axhline(
-        controller.position_set_point_deg, ls="--", color="red", label="Setpoint"
-    )
-    # Plot motor angle trajectory (with label)
-    ax.plot(
-        controller.timestamps,
-        controller.position_history,
-        color="black",
-        label="Motor Angle Trajectory",
-    )
-    ax.legend()
+    # # Plot setpoint of 90.0 angle trajectory (with label)
+    # ax.axhline(
+    #     controller.position_set_point_deg, ls="--", color="red", label="Setpoint"
+    # )
+    # # Plot motor angle trajectory (with label)
+    # ax.plot(
+    #     controller.timestamps,
+    #     controller.position_history,
+    #     color="black",
+    #     label="Motor Angle Trajectory",
+    # )
+    # ax.legend()
 
-    fig.savefig(fig_file_name)
+    # fig.savefig(fig_file_name)
 
-    plt.show()
+    # plt.show()
