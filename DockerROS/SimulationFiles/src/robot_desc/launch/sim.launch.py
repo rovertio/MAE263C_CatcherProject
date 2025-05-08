0db774c4eee6c3ee4f1248585f384d94bd21a7a1
rovertio <73990195+rovertio@users.noreply.github.com>
2025-05-08 01:37:00 -0700

    sending files into ignition

--- DockerROS/SimulationFiles/src/robot_desc/launch/sim.launch.py
+++ DockerROS/SimulationFiles/src/robot_desc/launch/sim.launch.py
@@ -3,11 +3,11 @@ from ament_index_python.packages import get_package_share_directory
 from ament_index_python import get_package_prefix
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+from launch.actions import ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 from launch_ros.actions import Node
-import xacro
 
 
 def generate_launch_description():
@@ -21,7 +21,6 @@ def generate_launch_description():
     # robot_description_raw = xacro.process_file(xacro_file).toxml()
 
     package_description = "robot_desc"
-    # robot_description_package = launch_ros.substitutions.FindPackageShare(package='robot_desc').find('robot_desc')
 
     pkg_share_path = os.pathsep + os.path.join(get_package_prefix(package_description), 'share')
     if 'GAZEBO_MODEL_PATH' in os.environ:
@@ -29,29 +28,40 @@ def generate_launch_description():
     else:
         os.environ['GAZEBO_MODEL_PATH'] =  pkg_share_path
 
-    ####### DATA INPUT END ##########
     print("Fetching URDF ==>")
-    #robot_desc_path = os.path.join(get_package_share_directory(package_description), "robot", urdf_file)
 
-    sdf_file_name = 'robot.sdf'
+    # sdf_file_name = 'simRobot.sdf'   
+    # sdf = os.path.join(
+    #     get_package_share_directory(package_description),
+    #     'robot',
+    #     sdf_file_name)
+    sdf_file_name = 'robot.urdf'   
     sdf = os.path.join(
         get_package_share_directory(package_description),
         'robot',
         sdf_file_name)
+    
+    world_file_name = 'simset.sdf'
+    world_sdf = os.path.join(
+        get_package_share_directory(package_description),
+        'simWorld',
+        world_file_name)
 
     with open(sdf, 'r') as infp:
         robot_desc = infp.read()
     
     print("Found UDRF")
 
-    # # Configure the node
-    # robot_state_publisher = Node(
-    #     package='robot_state_publisher',
-    #     executable='robot_state_publisher',
-    #     output='screen',
-    #     parameters=[{'robot_description': robot_desc,
-    #     'use_sim_time': True}] # add other parameters here if required
-    # )
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'robot.rviz')
+
+
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir])
 
     # Robot State Publisher
     robot_state_publisher = Node(
@@ -64,19 +74,21 @@ def generate_launch_description():
         output="screen"
     )
 
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+    gazebo = ExecuteProcess(
+            cmd=['ign', 'gazebo', world_sdf],
+            output='screen'
         )
 
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                     arguments=['-name', 'robot',
-                        '-topic', 'robot_description'
+                               '-topic', 'robot_description',
+                               #'-world', world_sdf
                                ],
                     output='screen')
 
     # Run the node
     return LaunchDescription([
+        rviz_node,
         gazebo,
         robot_state_publisher,
         spawn_entity
